87414960365786602f66755d92cbaadf56dc7572
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-04-10 14:15:03 +0530

    all

---
+++ src/index.js
@@ -0,0 +1,128 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import "./index.css"; 
+
+
+ReactDOM.render( 
+         <div>
+
+         <h1 className="heading_style"> 2k16 Batch CSE </h1>
+
+
+         <Card 
+           imgsrc="rishav.jpg"
+           title="Engineer"
+           name="Rishav"
+           link="https://www.instagram.com/rishav_raj1/"
+         />
+
+          <Card 
+           imgsrc="anshuman.jpg"
+           title="Engineer"
+           name="Anshuaman"
+           link="https://www.instagram.com/anshumanbadal/"
+         />
+
+          <Card 
+           imgsrc="dhiraj.jpg"
+           title="Engineer"
+           name="Dhiraj"
+           link="https://www.instagram.com/dhraj_p/"
+         />
+
+         <Card 
+           imgsrc="shilpi.jpg"
+           title="Engineer"
+           name="Shilpi"
+           link=""
+         />
+
+         <Card 
+           imgsrc="sakshi.jpg"
+           title="Engineer"
+           name="Sakshi"
+           link=""
+         />
+
+         <Card 
+           imgsrc="Rohit.jpg"
+           title="Engineer"
+           name="Rohit"
+           link=""
+         />
+
+         <Card 
+           imgsrc="avinash.jpg"
+           title="Engineer"
+           name="Avinash ji"
+           link=""
+         />
+
+         <Card 
+           imgsrc="kavya.jpg"
+           title="Engineer"
+           name="Kavya"
+           link=""
+         />
+
+         <Card 
+           imgsrc="shyam.jpg"
+           title="Engineer"
+           name="Shyam"
+           link=""
+         />
+
+         <Card 
+           imgsrc="chandan.jpg"
+           title="Engineer"
+           name="Chandan"
+           link=""
+         />
+
+         <Card 
+           imgsrc="ankit.jpg"
+           title="Engineer"
+           name="Ankit"
+           link=""
+         />
+
+         <Card 
+           imgsrc="suraj.jpg"
+           title="Engineer"
+           name="Suraj"
+           link=""
+         />
+
+         <Card 
+           imgsrc="devashish.jpg"
+           title="Engineer"
+           name="Devashish"
+           link=""
+         />
+
+         <Card 
+           imgsrc="ritesh.jpg"
+           title="Engineer"
+           name="Ritesh"
+           link=""
+         />
+
+         <Card 
+           imgsrc="ankit_himanshu.jpg"
+           title="Engineer"
+           name="Ankit Himanshu"
+           link=""
+         />
+
+         <Card 
+           imgsrc="diwakar.jpg"
+           title="Engineer"
+           name="Diwakar"
+           link=""
+         />
+
+         </div>,
+  document.getElementById('root')
+);
+
